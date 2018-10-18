4ece0e82547460622c6768f85c8d5af0cf366560
valuejoe <chiaohung82@gmail.com>
2018-10-18 22:36:46 +0800

    basic redux practice

--- src/js/index.js
+++ src/js/index.js
@@ -1,6 +1,32 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore } from 'redux'
 
+const reducer = (state='', action) => {
+	if(action.type='pps'){
+		return {index: action.index}
+	}
+};
+const init_state = {index : 1};
+
+let store = createStore(reducer, init_state );
+
+const action_01 = {
+	type:'pps',
+	index:100
+};
+
+function addvalue(value){
+	return{
+		type: 'ADD_value',
+		index: value
+	}
+};
+
+store.dispatch(addvalue(100))
+console.log(store.getState())
+store.dispatch(addvalue(10))
+console.log(store.getState())
 render( 
 	<div>hello world ouo/</div>,
 	document.getElementById('app')
